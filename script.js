78fda8baa7109c126d49e63cf5ab475fb4d6ec73
ayham <ay.matuk@gmail.com>
2025-09-27 19:54:33 +0300

    Первая версия: создана структура проекта

---
+++ script.js
@@ -0,0 +1,9 @@
+// Простой скрипт для начала
+console.log('Магазин запчастей запущен!');
+
+function showMessage() {
+    alert('Добро пожаловать в iFixParts!');
+}
+
+// Вызовем функцию при загрузке страницы
+window.onload = showMessage;
